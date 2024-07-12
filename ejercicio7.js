003689be5493f41e6dfc2dca0a9cc97c05ec85a2
cristian Ruiz <cristianwwe@hotmail.com>
2024-07-12 00:35:07 -0500

    correción ejercicio 7

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,19 +1,19 @@
 let nombre = prompt("Ingresa tu nombre:");
-let pase = prompt("¿Tienes pase VIP o normal?");
-let poseeEntrada = prompt("¿Tienes entrada? (si/no)");
+let pase = prompt("¿Tienes pase VIP o normal?").toLowerCase();
+let poseeEntrada = prompt("¿Tienes entrada? (si/no)").toLowerCase();
 
-if (nombre === "TuNombre" || pase.toLowerCase() === "vip") {
+if (nombre === "Cristian" || pase === "vip") {
     console.log("Bienvenido, " + nombre);
-} else if (poseeEntrada.toLowerCase() === "si") {
-    let usarEntrada = prompt("¿Deseas usar tu entrada? (si/no)");
-    if (usarEntrada.toLowerCase() === "si") {
+} else if (poseeEntrada === "si") {
+    let usarEntrada = prompt("¿Deseas usar tu entrada? (si/no)").toLowerCase();
+    if (usarEntrada === "si") {
         console.log("Bienvenido, " + nombre);
     } else {
         console.log("Despedida.");
     }
 } else {
-    let comprarEntrada = prompt("¿Deseas comprar una entrada? (si/no)");
-    if (comprarEntrada.toLowerCase() === "si") {
+    let comprarEntrada = prompt("¿Deseas comprar una entrada? (si/no)").toLowerCase();
+    if (comprarEntrada === "si") {
         let dinero = parseFloat(prompt("¿Cuánto dinero tienes?"));
         if (dinero >= 1000) {
             console.log("Venta exitosa. Bienvenido, " + nombre);
